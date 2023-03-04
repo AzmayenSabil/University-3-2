c7b1154fde976c35c4beeab1bb06c7216977d643
Azmayen Fayek Sabil <azmayenfayek@iut-dhaka.edu>
2023-03-04 20:51:15 +0600

    push

---
+++ SWE-4602-SD-lab/Microservice/demo/src/main/java/com/example/demo/EmployeeServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+//@EnableEurekaClient
+@EnableDiscoveryClient
+
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+
+	}
+}
