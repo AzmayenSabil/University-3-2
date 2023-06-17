8f015ad60ade4ef14df3ffa01881a2b5ca6cc791
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2023-06-17 22:48:13 +0600

    git push

--- CSE-4618-AI-lab/Lab-4/multiagent/multiAgents.py
+++ CSE-4618-AI-lab/Lab-4/multiagent/multiAgents.py
@@ -255,10 +255,68 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
 
     def getAction(self, gameState):
         """
-        Returns the minimax action using self.depth and self.evaluationFunction
+          Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        # Used only for pacman agent hence agentindex is always 0.
+        def maxLevel(gameState, depth, alpha, beta):
+            currDepth = depth + 1
+            if gameState.isWin() or gameState.isLose() or currDepth == self.depth:  # Terminal Test
+                return self.evaluationFunction(gameState)
+            maxvalue = -999999
+            actions = gameState.getLegalActions(0)
+            alpha1 = alpha
+            for action in actions:
+                successor = gameState.generateSuccessor(0, action)
+                maxvalue = max(maxvalue, minLevel(successor, currDepth, 1, alpha1, beta))
+                if maxvalue > beta:
+                    return maxvalue
+                alpha1 = max(alpha1, maxvalue)
+            return maxvalue
+
+        # For all ghosts.
+        def minLevel(gameState, depth, agentIndex, alpha, beta):
+            minvalue = 999999
+            if gameState.isWin() or gameState.isLose():  # Terminal Test
+                return self.evaluationFunction(gameState)
+            actions = gameState.getLegalActions(agentIndex)
+            beta1 = beta
+            for action in actions:
+                successor = gameState.generateSuccessor(agentIndex, action)
+                if agentIndex == (gameState.getNumAgents() - 1):
+                    minvalue = min(minvalue, maxLevel(successor, depth, alpha, beta1))
+                    if minvalue < alpha:
+                        return minvalue
+                    beta1 = min(beta1, minvalue)
+                else:
+                    minvalue = min(minvalue, minLevel(successor, depth, agentIndex + 1, alpha, beta1))
+                    if minvalue < alpha:
+                        return minvalue
+                    beta1 = min(beta1, minvalue)
+            return minvalue
+
+        # Alpha-Beta Pruning
+        actions = gameState.getLegalActions(0)
+        currentScore = -999999
+        returnAction = ''
+        alpha = -999999
+        beta = 999999
+        for action in actions:
+            nextState = gameState.generateSuccessor(0, action)
+            # Next level is a min level. Hence calling min for successors of the root.
+            score = minLevel(nextState, 0, 1, alpha, beta)
+            # Choosing the action which is Maximum of the successors.
+            if score > currentScore:
+                returnAction = action
+                currentScore = score
+            # Updating alpha value at root.
+            if score > beta:
+                return returnAction
+            alpha = max(alpha, score)
+        return returnAction
+
+        # util.raiseNotDefined()
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -267,13 +325,60 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
 
     def getAction(self, gameState):
         """
-        Returns the expectimax action using self.depth and self.evaluationFunction
+          Returns the expectimax action using self.depth and self.evaluationFunction
 
-        All ghosts should be modeled as choosing uniformly at random from their
-        legal moves.
+          All ghosts should be modeled as choosing uniformly at random from their
+          legal moves.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        # Used only for pacman agent hence agentindex is always 0.
+        def maxLevel(gameState, depth):
+            currDepth = depth + 1
+            if gameState.isWin() or gameState.isLose() or currDepth == self.depth:  # Terminal Test
+                return self.evaluationFunction(gameState)
+            maxvalue = -999999
+            actions = gameState.getLegalActions(0)
+            totalmaxvalue = 0
+            numberofactions = len(actions)
+            for action in actions:
+                successor = gameState.generateSuccessor(0, action)
+                maxvalue = max(maxvalue, expectLevel(successor, currDepth, 1))
+            return maxvalue
+
+        # For all ghosts.
+        def expectLevel(gameState, depth, agentIndex):
+            if gameState.isWin() or gameState.isLose():  # Terminal Test
+                return self.evaluationFunction(gameState)
+            actions = gameState.getLegalActions(agentIndex)
+            totalexpectedvalue = 0
+            numberofactions = len(actions)
+            for action in actions:
+                successor = gameState.generateSuccessor(agentIndex, action)
+                if agentIndex == (gameState.getNumAgents() - 1):
+                    expectedvalue = maxLevel(successor, depth)
+                else:
+                    expectedvalue = expectLevel(successor, depth, agentIndex + 1)
+                totalexpectedvalue = totalexpectedvalue + expectedvalue
+            if numberofactions == 0:
+                return 0
+            return float(totalexpectedvalue) / float(numberofactions)
+
+        # Root level action.
+        actions = gameState.getLegalActions(0)
+        currentScore = -999999
+        returnAction = ''
+        for action in actions:
+            nextState = gameState.generateSuccessor(0, action)
+            # Next level is a expect level. Hence calling expectLevel for successors of the root.
+            score = expectLevel(nextState, 0, 1)
+            # Choosing the action which is Maximum of the successors.
+            if score > currentScore:
+                returnAction = action
+                currentScore = score
+        return returnAction
+
+        # util.raiseNotDefined()
 
 def betterEvaluationFunction(currentGameState):
     """
